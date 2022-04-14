bccd14df6e8da1a072ab529d113da04ae3ab36dd
a_szczepanik <a.szczepanik@adform.com>
2022-04-14 13:40:55 +0200

    Add test for n = 1000

--- src/test/scala/MultipliesSpec.scala
+++ src/test/scala/MultipliesSpec.scala
@@ -1,10 +1,11 @@
+import Multiplies._
 import org.scalatest._
 import flatspec._
 import matchers._
 
 class MultipliesSpec extends AnyFlatSpec with should.Matchers {
 
-  def sumOfMultiplies(n: Int): Int = if (n == 10) 23 else 78
+  behavior of "Multiplies.sumOfMultiplies()"
 
   "sumOfMultiplies" should "return value of 23 after adding together multiplies of 3 and 5 (3, 5, 6, 9)" in {
     sumOfMultiplies(10) shouldBe 23
@@ -12,5 +13,9 @@ class MultipliesSpec extends AnyFlatSpec with should.Matchers {
 
   it should "return value of 98 after adding together multiplies of 3 and 5 (3, 5, 6, 9, 10, 12, 15, 18)" in {
     sumOfMultiplies(20) shouldBe 78
+  }
+
+  it should "return value of 98 after adding together multiplies of 3 and 5 (3, 5, 6, 9, 10, 12, 15, 18..., 999)" in {
+    sumOfMultiplies(1000) shouldBe 233168
   }
 }
